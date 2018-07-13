5ee5a92a2c3a89b8373ee6c84735f87c78bf8074
braulio <brlvldvlsmrtnz@gmail.com>
2018-07-13 14:13:41 +0200

    start writing code object generation

--- src/Scheme/Operations.hs
+++ src/Scheme/Operations.hs
@@ -1,7 +1,10 @@
 module Scheme.Operations where
 
+import qualified Data.ByteString as BS
 import Scheme.References.Types
 import Scheme.Types
+import Types
+import qualified Operations as Op
 
 data Operation = LoadVar MutableRef
                | SaveVar MutableRef
@@ -20,3 +23,29 @@ data CodeStruct = CodeStruct
                   , csArgumentNames :: [String]
                   }
 
+compileCS :: CodeStruct -> CodeObject
+compileCS (CodeStruct code consts free cell local argNames) = CodeObject
+  { argCount = length argNames
+  , kwOnlyArgCount = 0
+  , nLocals = length local + length cell
+  , stackSize = computeStackSize code
+  , flags = 0x40
+  , codeString = Op.getByteCode (toPyByteCode code)
+  , constants = undefined -- PTuple (toPyExpr <$> consts)
+  , names = PTuple []
+  , varNames = theVarNames
+  , filename = ""
+  , name = ""
+  , firstLineNo = 0
+  , lnotab = BS.empty
+  , freeVars = PTuple free
+  , cellVars = PTuple cell
+  }
+
+  where
+    computeStackSize :: [Operation] -> Int
+    computeStackSize _ = 0
+
+    toPyByteCode = undefined
+
+    theVarNames = undefined
